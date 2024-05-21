50bc48aab2f4aa268772c6030a478923c0c26d19
Beelz76 <91071691+Beelz76@users.noreply.github.com>
2024-05-21 22:43:48 +0300

    add files

---
+++ client/client.cpp
@@ -0,0 +1,119 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+
+void sendFile(SOCKET socket) {
+    std::cout << "Enter the file name: ";
+    std::string fileName;
+    std::cin >> fileName;
+
+    std::ifstream file(fileName, std::ios::binary);
+    if (!file.is_open()) {
+        std::cout << "File does not exist" << std::endl;
+        return;
+    }
+
+    send(socket, fileName.c_str(), fileName.size() + 1, 0);
+
+    char buffer[1024];
+
+    while (!file.eof()) {
+        file.read(buffer, sizeof(buffer));
+        int bytesRead = file.gcount();
+        send(socket, buffer, bytesRead, 0);
+    }
+
+    file.close();
+
+    std::cout << "File sent successfully" << std::endl;
+}
+
+void readFile(SOCKET socket) {
+    std::cout << "Enter the file name: ";
+    std::string fileName;
+    std::cin >> fileName;
+
+    send(socket, fileName.c_str(), fileName.size() + 1, 0);
+
+    char buffer[1024];
+
+    int bytesRead = recv(socket, buffer, sizeof(buffer), 0);
+    if (bytesRead <= 0) {
+        return;
+    }
+
+    std::cout << buffer;
+}
+
+int main()
+{
+    WSADATA ws;
+    struct sockaddr_in peer;
+    int clientSocket;
+    int rc;
+
+    peer.sin_family = AF_INET;
+    peer.sin_port = htons(1111);
+    peer.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (WSAStartup(0x0202, &ws) != 0)
+    {
+        printf("Error");
+        exit(1);
+    }
+
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        perror("Socket call failed");
+        exit(1);
+    }
+
+    rc = connect(clientSocket, (struct sockaddr*)&peer, sizeof(peer));
+    if (rc)
+    {
+        perror("Connect call failed");
+        exit(1);
+    }
+    else {
+        printf("Connected to server\n");
+    }
+
+    while (true) {
+        printf("\nChoose an action:\n");
+        printf("1.Send file\n");
+        printf("2.Read file\n");
+        printf("3.Exit\n");
+
+        int choice;
+        std::cin >> choice;
+
+        send(clientSocket, (char*)&choice, sizeof(int), 0);
+
+        switch (choice) {
+        case 1:
+            sendFile(clientSocket);
+            break;
+        case 2:
+            readFile(clientSocket);
+            break;
+        case 3:
+            printf("Exit the programm");
+            return 0;
+        default: 
+            printf("Invalid choice. Please try again.\n");
+            break;
+        }
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
+
---
+++ server/server.cpp
@@ -0,0 +1,129 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
+
+void receiveFile(SOCKET socket) {
+    char fileName[256];
+    recv(socket, fileName, sizeof(fileName), 0);
+
+    std::ofstream file(fileName, std::ios::binary);
+    if (!file.is_open()) {
+        std::cout << "Failed to create file\n" << std::endl;
+        return;
+    }
+
+    char buffer[1024];
+    int bytesReceived = recv(socket, buffer, sizeof(buffer), 0);
+
+    if (bytesReceived <= 0) {
+        return;
+    }
+    
+    file.write(buffer, bytesReceived);
+    file.close();
+
+    std::cout << "File received and saved as " << fileName << std::endl;
+}
+
+void showFileContent(SOCKET socket) {
+    char fileName[256];
+    recv(socket, fileName, sizeof(fileName), 0);
+
+    std::string content;
+
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        content = "Failed to open file";
+        send(socket, content.c_str(), content.size() + 1, 0);
+        return;
+    }
+
+    std::string line;
+    while (getline(file, line)) {
+        content += line + "\n";
+    }
+
+    send(socket, content.c_str(), content.size() + 1, 0);
+
+    std::cout << "File contents " << fileName << " sent to client " << std::endl;
+}
+
+int main()
+{
+    WSADATA ws;
+    struct sockaddr_in local;
+    int clientSocket;
+    int serverSocket;
+    int rc;
+
+    local.sin_family = AF_INET;
+    local.sin_port = htons(1111);
+    local.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (WSAStartup(0x0202, &ws) != 0)
+    {
+        printf("Error");
+        exit(1);
+    }
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0)
+    {
+        perror("Socket call failed");
+        exit(1);
+    }
+
+    rc = bind(serverSocket, (struct sockaddr*)&local, sizeof(local));
+    if (rc < 0)
+    {
+        perror("Bind call failure");
+        exit(1);
+    }
+
+    rc = listen(serverSocket, 5);
+    if (rc)
+    {
+        perror("Listen call failed");
+        exit(1);
+    }
+
+    clientSocket = accept(serverSocket, NULL, NULL);
+    if (clientSocket < 0)
+    {
+        perror("Accept call failed");
+        exit(1);
+    }
+    else {
+        printf("Client connected\n");
+    }
+
+    while (true) {
+        int receivedChoice;
+        recv(clientSocket, (char*)&receivedChoice, sizeof(receivedChoice), 0);
+
+        switch (receivedChoice) {
+        case 1:
+            receiveFile(clientSocket);
+            break;
+        case 2:
+            showFileContent(clientSocket);
+            break;
+        case 3: 
+            return 0;
+        default: 
+            printf("Invalid choice\n");
+            break;
+        }  
+    }
+
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
